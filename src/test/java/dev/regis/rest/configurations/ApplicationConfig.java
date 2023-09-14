c875f8742f938bd7ad4a9d7856b6fa7e58e01706
regis-amaral <amaral.regis@gmail.com>
2023-09-13 20:14:23 -0400

    testes SpecieService

---
+++ src/test/java/dev/regis/rest/configurations/ApplicationConfig.java
@@ -0,0 +1,15 @@
+package dev.regis.rest.configurations;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        return mapper;
+    }
+}
