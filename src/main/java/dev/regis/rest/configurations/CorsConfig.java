e49a7993a7da3b2ffd09788d13d9cf7309eab811
Regis Amaral <amaral.regis@gmail.com>
2023-10-13 18:52:51 -0300

    configuração CORS

---
+++ src/main/java/dev/regis/rest/configurations/CorsConfig.java
@@ -0,0 +1,17 @@
+package dev.regis.rest.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permitir todos os origens (substitua com seus domínios específicos se necessário)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
